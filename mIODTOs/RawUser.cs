d4ef5365d0f3f103ef1defa604eccb19600dc1de
Oliver Salvaterra <Spaceballs12345@Icloud.com>
2022-08-01 16:02:31 -0700

    added some user side data types

---
+++ mIODTOs/RawUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mIODTOs
+{
+    public class RawUser
+    {
+        public string Username;
+        public string Password;
+    }
+}
---
+++ mIODTOs/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mIODTOs
+{
+    public class UserData
+    {
+        public string devName;
+        public int value;
+        public DateTime createdOn;
+    }
+}
